f38ed4e6e01d499ebc74789656a82ff28bb5e1f4
Aditya Pandey <adityapandeyadp@gmail.com>
2025-01-31 13:02:27 +0530

    good for boal

--- src/app/Form/page.tsx
+++ src/app/Form/page.tsx
@@ -1,12 +1,10 @@
 import App from "@/components/form-builder/intro-form";
-import MedicalInformationForm from "@/components/form-builder/second-form";
 // import Second from "@/components/form-builder/second-form";
 
 function Page() {
   return (
     <>
       <App />
-      <MedicalInformationForm />
     </>
   );
 }
